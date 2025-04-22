ffb56c91af912d449934f2333c725d220a750185
t4ni_XP <gutchi-t@outlook.com>
2025-04-23 08:52:26 +0900

    make map/edit

---
+++ src/app/map/edit/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { TextField, Stack, Button, Box, Typography } from "@mui/material";
+import { useState } from "react";
+
+export default function MapForm({ defaultValues = {}, onSubmit }) {
+  return (
+    <Box m={3} sx={{ maxWidth: "md", margin: "auto" }}>
+      <Stack spacing={2} mt={3}>
+        <Typography>地図画像*</Typography>
+        <Box sx={{ pl: "24px" }}>
+          <input
+            id="user-profile-icon"
+            // onChange={handleUploadImage}
+            type="file"
+            accept="image/png, image/jpeg"
+            name="image"
+            hidden
+          />
+          <label htmlFor="user-profile-icon">
+            <Typography>画像を選択する</Typography>
+          </label>
+        </Box>
+        <Typography>タイトル*</Typography>
+        <TextField
+          name="title"
+          // label="タイトル"
+          // value={formData.title}
+          // onChange={handleChange}
+          fullWidth
+          required
+        />
+        <Typography>大会名</Typography>
+        <TextField
+          name="title"
+          // label="タイトル"
+          // value={formData.title}
+          // onChange={handleChange}
+        />
+        <Typography>コメント</Typography>
+        <TextField
+          name="comment"
+          // label="コメント"
+          // value={formData.comment}
+          // onChange={handleChange}
+          multiline
+          rows={4}
+          fullWidth
+        />
+        <Typography>日付</Typography>
+        <Box>
+          <TextField
+            name="date"
+            // label="日付"
+            type="date"
+            // value={formData.date}
+            // onChange={handleChange}
+          />
+        </Box>
+        <Button>保存</Button>
+      </Stack>
+    </Box>
+  );
+}
