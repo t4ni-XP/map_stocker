d3da871e92a1214819c4982cb4d9f4e4a673056b
t4ni_XP <gutchi-t@outlook.com>
2025-04-23 08:59:26 +0900

    fix lint

--- src/app/map/edit/page.tsx
+++ src/app/map/edit/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { TextField, Stack, Button, Box, Typography } from "@mui/material";
-import { useState } from "react";
+// import { useState } from "react";
 
-export default function MapForm({ defaultValues = {}, onSubmit }) {
+export default function MapForm() {
   return (
     <Box m={3} sx={{ maxWidth: "md", margin: "auto" }}>
       <Stack spacing={2} mt={3}>
