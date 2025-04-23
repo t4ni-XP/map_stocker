873098cb0c8d850c730a9f93cea9c98dfbfb5425
t4ni_XP <gutchi-t@outlook.com>
2025-04-23 17:34:05 +0900

    fix

--- src/app/map/[mapId]/page.tsx
+++ src/app/map/[mapId]/page.tsx
@@ -4,7 +4,7 @@ import prisma from "@/lib/prisma";
 import { notFound } from "next/navigation";
 import Image from "next/image";
 
-export default async function MapId({ params }: any) {
+export default async function MapId({ params }) {
   const { mapId } = params;
   const mapData = await prisma.mapImage.findFirst({
     select: {
