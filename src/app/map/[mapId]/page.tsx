389aea69248b680688348acaddb6beb3a087718a
t4ni_XP <gutchi-t@outlook.com>
2025-04-23 09:01:37 +0900

    fix module

---
+++ src/app/map/[mapId]/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { TextField, Stack, Button, Box, Typography } from "@mui/material";
+// import { useState } from "react";
+
+export default function MapForm() {
+  return <Box m={3} sx={{ maxWidth: "md", margin: "auto" }}></Box>;
+}
