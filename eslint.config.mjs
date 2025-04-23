ea37388b93fc2869136bd7ed4e2ec60315d82c6e
t4ni_XP <gutchi-t@outlook.com>
2025-04-23 09:16:20 +0900

    fix lint and add unused imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
+// eslint.config.js
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+// ↓ ここを require から import に変更！
+import pluginUnusedImports from "eslint-plugin-unused-imports";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +13,30 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  // 既存の extend 設定
   ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+
+  // unused-imports プラグイン＋ルールの追加
+  {
+    plugins: {
+      // 名前をキーに、import したオブジェクトを渡す
+      "unused-imports": pluginUnusedImports,
+    },
+    rules: {
+      // 未使用 import を自動削除
+      "unused-imports/no-unused-imports": "error",
+      // 未使用変数を警告
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
--- src/app/map/[mapId]/page.tsx
+++ src/app/map/[mapId]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Box } from "@mui/material";
-// import { useState } from "react";
 
 export default function MapForm() {
   return <Box m={3} sx={{ maxWidth: "md", margin: "auto" }}></Box>;
